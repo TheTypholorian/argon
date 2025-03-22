d29400ae7a13af5fbae009345fb31160b7ead13d
evan <evanpk2020@gmail.com>
2025-03-22 15:26:21 -0600

    collections work, giving up

--- test.cpp
+++ test.cpp
@@ -1,47 +1,22 @@
-#include <glew.h>
-#include <glfw3.h>
+#include "array.hpp"
 #include <iostream>
 
 using namespace std;
+using namespace H;
 
 int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+	Array<huint> array = Array<huint>(3);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	array.data[0] = 1;
+	array.data[1] = 5;
+	array.data[2] = 3;
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW Example", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+	array.add(5);
+	array.add(4);
 
-    glfwMakeContextCurrent(window);
-
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    int versionMajor, versionMinor;
-    glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
-    glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
-    cout << "OpenGL Version: " << versionMajor << "." << versionMinor << endl;
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
+	for (huint t : array) {
+		cout << t << " ";
+	}
 
 	return 0;
 }
